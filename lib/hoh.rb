fa515cbf9f46ba36580c55dbc4be980b2ffd863b
Ben Dunjay <bendunjay@outlook.com>
2020-02-13 23:40:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -39,26 +39,13 @@ def naming_system
             :sub_category => {:label => "Family",
               :sub_category => {:label => "Genus",
                  :sub_category => {:label => "Species"
-                      :sub_category => {:label => nil}
-                      
-      }
-              
-      
-      
-    }
-      
-      
-    }
-    
-      
-      
-    } 
-    }
-    }
+                      :sub_category => {}
+                 }
+}
   }
-  
-  
-  
+          } 
+        }
+      }
   # Remember:
   #  Kingdom
   #  Phylum
